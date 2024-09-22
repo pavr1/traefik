13a2a3defad67e5553ea0afa4f07cddf19eef596
pavr1Fintron <pablo@fintron.co>
2024-09-22 12:41:04 -0600

    adding svc1

--- svcs/svc1/go.mod
+++ svcs/svc1/go.mod
@@ -1,3 +1,5 @@
 module github.com/traefik/svcs/svc1
 
 go 1.23.1
+
+require github.com/gorilla/mux v1.8.1
--- svcs/svc1/main.go
+++ svcs/svc1/main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/pavr1/people/go/pkg/mod/gopkg.in/!data!dog/dd-trace-go.v1@v1.64.0/contrib/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	})
-	http.ListenAndServe(":80", router)
+	router.HandleFunc("/svc1/endpoint1", endpoint1).Methods("GET")
+	router.HandleFunc("/svc1/endpoint2", endpoint2).Methods("GET")
+	router.HandleFunc("/svc1/endpoint3", endpoint3).Methods("GET")
+
+	err := http.ListenAndServe(":9000", router)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func endpoint1(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+	fmt.Fprintf(w, "svc1.endpoint1")
+}
+
+func endpoint2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "svc1.endpoint2")
+}
+
+func endpoint3(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "svc1.endpoint3")
+}
